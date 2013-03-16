114a3a09163c54de9ec9dc38d7ee8c89175ffcd8
Christian Aistleitner <christian@quelltextlich.at>
2013-03-16 17:03:41 +0100

    Add support for schema 77
    Change-Id: I5a71641b1abd3d5cd0f40b25e1c29a9e257e4b01

---
+++ src/main/java/com/google/gerrit/plugins/database/Schema77DatabaseDeleteHandler.java
@@ -0,0 +1,38 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.database;
+
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.inject.Inject;
+
+public class Schema77DatabaseDeleteHandler
+    extends Schema73DatabaseDeleteHandler {
+  private final ReviewDb db;
+
+  @Inject
+  public Schema77DatabaseDeleteHandler(ReviewDb db) {
+    super(db);
+    this.db = db;
+  }
+
+  @Override
+  public void assertCanDelete(Project project) throws Exception {
+    if (db.submoduleSubscriptions().bySuperProjectProject(project.getNameKey())
+        .iterator().hasNext()) {
+      throw new Exception("Project has subscribed submodules.");
+    }
+  }
+}
