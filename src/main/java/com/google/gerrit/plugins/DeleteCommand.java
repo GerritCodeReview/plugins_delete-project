8e0168b028e8f3a8a6bdaeefbaf9db16eba17584
Christian Aistleitner <christian@quelltextlich.at>
2013-07-01 01:26:30 +0200

    Split off reusable way to delete projects from Ssh command
    The main method to delete projects was wrapped in an
    SshCommand. Hence, if other plugins wanted to reuse the project
    deletion implementation, they had to set up input streams, output
    streams, and all other parameters an SshCommand relies on. We isolate
    the main project deletion logic into the separata DeleteProject class
    to allow easier reuse.
    
    Change-Id: I1052884daf19fdf5dd0f75b81d05bfe7bd34fc92

--- src/main/java/com/google/gerrit/plugins/DeleteCommand.java
+++ src/main/java/com/google/gerrit/plugins/DeleteCommand.java
@@ -14,26 +14,12 @@
 
 package com.google.gerrit.plugins;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Collection;
-
-import org.eclipse.jgit.errors.RepositoryNotFoundException;
-import org.eclipse.jgit.lib.Config;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.lib.RepositoryCache;
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.Option;
 
 import com.google.gerrit.common.data.GlobalCapability;
 import com.google.gerrit.extensions.annotations.RequiresCapability;
-import com.google.gerrit.plugins.database.DatabaseDeleteHandler;
 import com.google.gerrit.reviewdb.client.Project;
-import com.google.gerrit.server.config.AllProjectsNameProvider;
-import com.google.gerrit.server.config.GerritServerConfig;
-import com.google.gerrit.server.config.SitePaths;
-import com.google.gerrit.server.git.GitRepositoryManager;
-import com.google.gerrit.server.project.ProjectCache;
 import com.google.gerrit.server.project.ProjectControl;
 import com.google.gerrit.sshd.SshCommand;
 import com.google.inject.Inject;
@@ -49,137 +35,23 @@ public final class DeleteCommand extends SshCommand {
   @Option(name = "--force", usage = "delete the project even if it has open changes")
   private boolean force = false;
 
-  private final File gitDir;
-
-  private final ProjectCache projectCache;
-
-  private final GitRepositoryManager repoManager;
-
-  private final SitePaths site;
-
-  private final DatabaseDeleteHandler databaseDeleteHandler;
+  private final DeleteProject.Factory deleteProjectFactory;
 
   @Inject
-  protected DeleteCommand(ProjectCache projectCache,
-      GitRepositoryManager repoManager, SitePaths site,
-      @GerritServerConfig Config cfg,
-      DatabaseDeleteHandler databaseDeleteHandler) {
-    gitDir = site.resolve(cfg.getString("gerrit", null, "basePath"));
-    this.projectCache = projectCache;
-    this.repoManager = repoManager;
-    this.site = site;
-    this.databaseDeleteHandler = databaseDeleteHandler;
+  public DeleteCommand(DeleteProject.Factory deleteProjectFactory) {
+    this.deleteProjectFactory = deleteProjectFactory;
   }
 
   @Override
   public void run() throws UnloggedFailure, Failure, Exception {
-    final Project project = projectControl.getProject();
-    final String projectName = project.getName();
-
-    // Don't let people delete All-Projects, that's stupid
-    if (projectName.endsWith(AllProjectsNameProvider.DEFAULT)) {
-      throw new UnloggedFailure("Perhaps you meant to rm -fR " + site.site_path);
-    }
+    Project project = projectControl.getProject();
+    DeleteProject deleteProject = deleteProjectFactory.create(project,
+        yesReallyDelete, force);
 
     try {
-      databaseDeleteHandler.assertCanDelete(project);
-    } catch (Exception e) {
-      throw new UnloggedFailure("Cannot delete project " + projectName + ": "
-          + e.getMessage());
-    }
-
-    if (!yesReallyDelete) {
-      StringBuilder msgBuilder = new StringBuilder();
-      msgBuilder.append("Really delete ");
-      msgBuilder.append(projectName);
-      msgBuilder.append("?\n");
-      msgBuilder.append("This is an operation which permanently deletes");
-      msgBuilder.append("data. This cannot be undone!\n");
-      msgBuilder.append("If you are sure you wish to delete this project, ");
-      msgBuilder.append("re-run\n");
-      msgBuilder.append("with the --yes-really-delete flag.\n");
-      throw new UnloggedFailure(msgBuilder.toString());
-    }
-
-    if (!force) {
-      Collection<String> warnings = databaseDeleteHandler.getWarnings(project);
-      if (warnings != null && !warnings.isEmpty()) {
-        StringBuilder msgBuilder = new StringBuilder();
-        msgBuilder.append("There are warnings against deleting ");
-        msgBuilder.append(projectName);
-        msgBuilder.append(":\n");
-        for (String warning: warnings) {
-          msgBuilder.append(" * ");
-          msgBuilder.append(warning);
-          msgBuilder.append("\n");
-        }
-        msgBuilder.append("To really delete ");
-        msgBuilder.append(projectName);
-        msgBuilder.append(", re-run with the --force flag.");
-        throw new UnloggedFailure(msgBuilder.toString());
-      }
-    }
-
-    databaseDeleteHandler.delete(project);
-    deleteFromDisk(project);
-
-    // Clean up the cache
-    projectCache.remove(project);
-  }
-
-  private void deleteFromDisk(Project project) throws IOException,
-      RepositoryNotFoundException, UnloggedFailure {
-    // Remove from the jgit cache
-    final Repository repository = repoManager.openRepository(project
-        .getNameKey());
-    if (repository == null) {
-      throw new UnloggedFailure("There was an error finding the project.");
-    }
-
-    repository.close();
-    RepositoryCache.close(repository);
-
-    // Delete the repository from disk
-    File parentFile = repository.getDirectory().getParentFile();
-    if (!recursiveDelete(repository.getDirectory())) {
-      throw new UnloggedFailure("Error trying to delete "
-          + repository.getDirectory().getAbsolutePath());
-    }
-
-    // Delete parent folders while they are (now) empty
-    recursiveDeleteParent(parentFile, gitDir);
-  }
-
-  /**
-   * Recursively delete the specified file and all of its contents.
-   *
-   * @return true on success, false if there was an error.
-   */
-  private boolean recursiveDelete(File file) {
-    if (file.isDirectory()) {
-      for (File f : file.listFiles()) {
-        if (!recursiveDelete(f)) {
-          return false;
-        }
-      }
-    }
-    return file.delete();
-  }
-
-  /**
-   * Recursively delete the specified file and its parent files until we hit the
-   * file {@code Until} or the parent file is populated. This is used when we
-   * have a tree structure such as a/b/c/d.git and a/b/e.git - if we delete
-   * a/b/c/d.git, we no longer need a/b/c/.
-   */
-  private void recursiveDeleteParent(File file, File until) {
-    if (file.equals(until)) {
-      return;
-    }
-    if (file.listFiles().length == 0) {
-      File parent = file.getParentFile();
-      file.delete();
-      recursiveDeleteParent(parent, until);
+      deleteProject.run();
+    } catch (DeleteProjectFailedException e) {
+      throw new UnloggedFailure(e.getMessage());
     }
   }
 }
---
+++ src/main/java/com/google/gerrit/plugins/DeleteProject.java
@@ -0,0 +1,185 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryCache;
+
+import com.google.gerrit.plugins.database.DatabaseDeleteHandler;
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.config.AllProjectsNameProvider;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.gerrit.server.git.GitRepositoryManager;
+import com.google.gerrit.server.project.ProjectCache;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+
+public final class DeleteProject {
+  public interface Factory {
+    DeleteProject create(@Assisted Project project,
+        @Assisted("confirmed") boolean confirmed,
+        @Assisted("force") boolean force);
+  }
+
+  private Project project;
+  private boolean confirmed;
+  private boolean force;
+
+  private final File gitDir;
+
+  private final ProjectCache projectCache;
+
+  private final GitRepositoryManager repoManager;
+
+  private final SitePaths site;
+
+  private final DatabaseDeleteHandler databaseDeleteHandler;
+
+  @Inject
+  public DeleteProject(ProjectCache projectCache,
+      GitRepositoryManager repoManager, SitePaths site,
+      @GerritServerConfig Config cfg,
+      DatabaseDeleteHandler databaseDeleteHandler,
+      @Assisted Project project,
+      @Assisted("confirmed") boolean confirmed,
+      @Assisted("force") boolean force) {
+    gitDir = site.resolve(cfg.getString("gerrit", null, "basePath"));
+    this.projectCache = projectCache;
+    this.repoManager = repoManager;
+    this.site = site;
+    this.databaseDeleteHandler = databaseDeleteHandler;
+
+    this.project = project;
+    this.confirmed = confirmed;
+    this.force = force;
+  }
+
+  public void run() throws DeleteProjectFailedException, Exception {
+    final String projectName = project.getName();
+
+    // Don't let people delete All-Projects, that's stupid
+    if (projectName.endsWith(AllProjectsNameProvider.DEFAULT)) {
+      throw new DeleteProjectFailedException("Perhaps you meant to rm -fR " + site.site_path);
+    }
+
+    try {
+      databaseDeleteHandler.assertCanDelete(project);
+    } catch (Exception e) {
+      throw new DeleteProjectFailedException("Cannot delete project " + projectName + ": "
+          + e.getMessage());
+    }
+
+    if (!confirmed) {
+      StringBuilder msgBuilder = new StringBuilder();
+      msgBuilder.append("Really delete ");
+      msgBuilder.append(projectName);
+      msgBuilder.append("?\n");
+      msgBuilder.append("This is an operation which permanently deletes");
+      msgBuilder.append("data. This cannot be undone!\n");
+      msgBuilder.append("If you are sure you wish to delete this project, ");
+      msgBuilder.append("re-run\n");
+      msgBuilder.append("with the --yes-really-delete flag.\n");
+      throw new DeleteProjectFailedException(msgBuilder.toString());
+    }
+
+    if (!force) {
+      Collection<String> warnings = databaseDeleteHandler.getWarnings(project);
+      if (warnings != null && !warnings.isEmpty()) {
+        StringBuilder msgBuilder = new StringBuilder();
+        msgBuilder.append("There are warnings against deleting ");
+        msgBuilder.append(projectName);
+        msgBuilder.append(":\n");
+        for (String warning: warnings) {
+          msgBuilder.append(" * ");
+          msgBuilder.append(warning);
+          msgBuilder.append("\n");
+        }
+        msgBuilder.append("To really delete ");
+        msgBuilder.append(projectName);
+        msgBuilder.append(", re-run with the --force flag.");
+        throw new DeleteProjectFailedException(msgBuilder.toString());
+      }
+    }
+
+    databaseDeleteHandler.delete(project);
+    deleteFromDisk(project);
+
+    // Clean up the cache
+    projectCache.remove(project);
+  }
+
+  private void deleteFromDisk(Project project) throws IOException,
+      RepositoryNotFoundException, DeleteProjectFailedException {
+    // Remove from the jgit cache
+    final Repository repository = repoManager.openRepository(project
+        .getNameKey());
+    if (repository == null) {
+      throw new DeleteProjectFailedException("There was an error finding the project.");
+    }
+
+    repository.close();
+    RepositoryCache.close(repository);
+
+    // Delete the repository from disk
+    File parentFile = repository.getDirectory().getParentFile();
+    if (!recursiveDelete(repository.getDirectory())) {
+      throw new DeleteProjectFailedException("Error trying to delete "
+          + repository.getDirectory().getAbsolutePath());
+    }
+
+    // Delete parent folders while they are (now) empty
+    recursiveDeleteParent(parentFile, gitDir);
+  }
+
+  /**
+   * Recursively delete the specified file and all of its contents.
+   *
+   * @return true on success, false if there was an error.
+   */
+  private boolean recursiveDelete(File file) {
+    if (file.isDirectory()) {
+      for (File f : file.listFiles()) {
+        if (!recursiveDelete(f)) {
+          return false;
+        }
+      }
+    }
+    return file.delete();
+  }
+
+  /**
+   * Recursively delete the specified file and its parent files until we hit the
+   * file {@code Until} or the parent file is populated. This is used when we
+   * have a tree structure such as a/b/c/d.git and a/b/e.git - if we delete
+   * a/b/c/d.git, we no longer need a/b/c/.
+   */
+  private void recursiveDeleteParent(File file, File until) {
+    if (file.equals(until)) {
+      return;
+    }
+    if (file.listFiles().length == 0) {
+      File parent = file.getParentFile();
+      file.delete();
+      recursiveDeleteParent(parent, until);
+    }
+  }
+}
