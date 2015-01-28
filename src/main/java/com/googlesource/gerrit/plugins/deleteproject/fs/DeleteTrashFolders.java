a9d6e484980650d6b7aafe346722a458ad0d96c1
Christian Halstrick <christian.halstrick@sap.com>
2015-01-28 10:10:49 +0100

    Remove trash folders when Gerrit starts
    Project deletion now first renames the repository folder and then tries to
    delete it.  If this deletion fails the filesystem will still contain the
    renamed and halfways deleted repository folders.  This commit teaches gerrit
    to try to delete this trash folders during startup.  Chances are high that
    during startup of gerrit nobody holds open handles on this file and the
    deletion will succeed.
    
    Change-Id: I48fd9a7096b18e613619b73250ac47117e99f57c

---
+++ src/main/java/com/googlesource/gerrit/plugins/deleteproject/fs/DeleteTrashFolders.java
@@ -0,0 +1,106 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.googlesource.gerrit.plugins.deleteproject.fs;
+
+import com.google.gerrit.extensions.events.LifecycleListener;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.inject.Inject;
+
+import org.eclipse.jgit.lib.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class DeleteTrashFolders implements LifecycleListener {
+  private static final Logger log = LoggerFactory.getLogger(DeleteTrashFolders.class);
+  private File gitDir;
+
+  @Inject
+  public DeleteTrashFolders(SitePaths site, @GerritServerConfig Config cfg) {
+    gitDir = site.resolve(cfg.getString("gerrit", null, "basePath"));
+  }
+
+  class TrashFolderRemover extends SimpleFileVisitor<Path> {
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+        throws IOException {
+      String fName = dir.getFileName().toString();
+      if (fName.endsWith(".deleted")
+          && fName.matches(".*\\d{13}.deleted$")) {
+        log.warn("Will delete this folder: {}", dir);
+        recursiveDelete(dir);
+        return FileVisitResult.SKIP_SUBTREE;
+      }
+      return super.preVisitDirectory(dir, attrs);
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+        throws IOException {
+      if (attrs.isRegularFile() && file.getFileName().toString().equals("HEAD") && file.getParent().toString().endsWith(".git")) {
+        return FileVisitResult.SKIP_SIBLINGS;
+      }
+      return super.visitFile(file, attrs);
+    }
+  }
+
+  /**
+   * Recursively delete the specified file and all of its contents.
+   *
+   * @throws IOException
+   */
+  private void recursiveDelete(Path file) throws IOException {
+    Files.walkFileTree(file, new SimpleFileVisitor<Path>() {
+      @Override
+      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+          throws IOException {
+        Files.delete(file);
+        return FileVisitResult.CONTINUE;
+      }
+
+      @Override
+      public FileVisitResult postVisitDirectory(Path dir, IOException e)
+          throws IOException {
+        if (e != null) {
+          throw e;
+        }
+        Files.delete(dir);
+        return FileVisitResult.CONTINUE;
+      }
+    });
+  }
+
+
+  @Override
+  public void start() {
+      try {
+        Files.walkFileTree(gitDir.toPath(), new TrashFolderRemover());
+      } catch (IOException e) {
+        log.warn("Exception occured while trying to delete trash folders", e);
+      }
+  }
+
+  @Override
+  public void stop() {
+  }
+}
+
