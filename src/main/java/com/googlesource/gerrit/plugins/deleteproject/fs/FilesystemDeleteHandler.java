GerritCodeReview/plugins_delete-project

commit bc4834c0eac09a97c271fda0b1e006426d8bc135

Changes to prevent 500 error in UI when repo files not deleted.
Refactored to not require repository object for delete method.
This allows repository to be fully closed before delete.

Changed exception throw to log.warn instead.

Code already existed to move repo to trash, and clean up trash
on next restart.

Change-Id: Ic9d9cca45db17b237348898f00e31633d103677a

src/main/java/com/googlesource/gerrit/plugins/deleteproject/fs/FilesystemDeleteHandler.java
@@ -73,9 +73,10 @@ public class FilesystemDeleteHandler {
     // Remove from the jgit cache
     Repository repository =
         repoManager.openRepository(project.getNameKey());
+    File repoParentFile = repository.getDirectory().getParentFile();
     cleanCache(repository);
     if (!preserveGitRepository) {
-      deleteGitRepository(project.getNameKey(), repository);
+      deleteGitRepository(project.getNameKey(), repoParentFile);
     }
   }
 
@@ -106,19 +107,19 @@ public class FilesystemDeleteHandler {
   }
 
   private void deleteGitRepository(final Project.NameKey project,
-      final Repository repository) throws IOException {
+      final File repoParentFile) throws IOException {
     // Delete the repository from disk
-    File parentFile = repository.getDirectory().getParentFile();
-
-    Path trash = moveToTrash(repository.getDirectory().toPath(), project);
+    Path trash = moveToTrash(repoParentFile.toPath(), project);
     try {
       recursiveDelete(trash);
     } catch (IOException e) {
-      throw new IOException("Error trying to delete " + trash, e);
+      // Only log if delete failed - repo already moved to trash.
+      // Otherwise, listeners are never called.
+      log.warn("Error trying to delete " + trash, e);
     }
 
-    // Delete parent folders while they are (now) empty
-    recursiveDeleteParent(parentFile, gitDir);
+    // Delete parent folders if they are (now) empty
+    recursiveDeleteParent(repoParentFile, gitDir);
 
     // Send an event that the repository was deleted
     ProjectDeletedListener.Event event = new ProjectDeletedListener.Event() {

commit 9eb4988bc9c6ddf4a939497b1018b77d0c92104c

Wrong level File object for successful delete.
Because the File object passed to the deleteGitRepository() routine was
parent instead of the respository to be deleted, the move and delete
would usually simply fail to happen.  Untested, but it could have been
a VERY serious problem with git repos that were not directly under the
git root, as the code would wind up deleting the parent instead of the
target repo, which could delete multiple repos if there were siblings at
2nd or lower levels.

Problem was introduced in change
bc4834c0eac09a97c271fda0b1e006426d8bc135

Change-Id: I0b0859d59bde2d8eaba681a6d68d2d2ceeacf84c

src/main/java/com/googlesource/gerrit/plugins/deleteproject/fs/FilesystemDeleteHandler.java
@@ -73,10 +73,10 @@ public class FilesystemDeleteHandler {
     // Remove from the jgit cache
     Repository repository =
         repoManager.openRepository(project.getNameKey());
-    File repoParentFile = repository.getDirectory().getParentFile();
+    File repoFile = repository.getDirectory();
     cleanCache(repository);
     if (!preserveGitRepository) {
-      deleteGitRepository(project.getNameKey(), repoParentFile);
+      deleteGitRepository(project.getNameKey(), repoFile);
     }
   }
 
@@ -107,9 +107,9 @@ public class FilesystemDeleteHandler {
   }
 
   private void deleteGitRepository(final Project.NameKey project,
-      final File repoParentFile) throws IOException {
+      final File repoFile) throws IOException {
     // Delete the repository from disk
-    Path trash = moveToTrash(repoParentFile.toPath(), project);
+    Path trash = moveToTrash(repoFile.toPath(), project);
     try {
       recursiveDelete(trash);
     } catch (IOException e) {
@@ -119,7 +119,7 @@ public class FilesystemDeleteHandler {
     }
 
     // Delete parent folders if they are (now) empty
-    recursiveDeleteParent(repoParentFile, gitDir);
+    recursiveDeleteParent(repoFile.getParentFile(), gitDir);
 
     // Send an event that the repository was deleted
     ProjectDeletedListener.Event event = new ProjectDeletedListener.Event() {