35209f3feae079a78b531ed69509f93f488d784b
Paladox none <thomasmulhall410@yahoo.com>
2017-11-11 17:30:15 +0000

    Fix support for polygerrit
    Change-Id: I66d926265a9eeddbc3a081ad1fdb35c9a8843ce7

---
+++ src/main/resources/static/project-command-delete-project.js
@@ -0,0 +1,44 @@
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'repo-command-delete-project',
+
+    attached() {
+      this.hidden = this.config &&
+        !this.config.actions['delete-project~delete'] ||
+        !this.config ||
+        this.projectName === 'All-Projects';
+    },
+
+    _handleCommandTap() {
+      this.$.deleteRepoOverlay.open();
+    },
+
+    _handleCloseDeleteRepo() {
+      this.$.deleteRepoOverlay.close();
+    },
+
+    _handleDeleteRepo() {
+      if (!this.repoName) { return; }
+
+      const forceDelete = this.$.forceDeleteOpenChangesCheckBox.checked;
+      const keepRepo = this.$.preserveGitRepoCheckBox.checked;
+
+      const encodeName = encodeURIComponent(this.repoName);
+      const pluginRestApi = '/projects/' + encodeName +
+          '/delete-project~delete';
+
+      const json = {
+        force: forceDelete,
+        preserve: keepRepo
+      };
+
+      return Gerrit.post(pluginRestApi, json, projectDeleted => {
+        this._handleCloseDeleteRepo();
+
+        page.show('/admin/repos/');
+      });
+    },
+  });
+})();
