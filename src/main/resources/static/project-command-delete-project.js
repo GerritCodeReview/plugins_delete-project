f021ef21c1d2d05587cfe8bacf923d72af17e874
Paladox none <thomasmulhall410@yahoo.com>
2017-11-11 17:30:15 +0000

    Fix support for polygerrit
    Change-Id: I66d926265a9eeddbc3a081ad1fdb35c9a8843ce7

---
+++ src/main/resources/static/project-command-delete-project.js
@@ -0,0 +1,35 @@
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'project-command-delete-project',
+
+    attached() {
+      console.log(this.projectName);
+      console.log(this.config);
+      this.hidden = this.projectName === 'All-Projects';
+    },
+
+    _handleCommandTap() {
+      this.$.deleteProjectOverlay.open();
+    },
+
+    _handleCloseDeleteProject() {
+      this.$.deleteProjectOverlay.close();
+    },
+
+    handleDeleteProject() {
+      const forceDelete = this.$.forceDeleteOpenChangesCheckBox.checked;
+      const keepRepo = this.$.preserveGitRepoCheckBox.checked;
+      return this.$.restAPI.createChange(this.projectName, forceDelete,
+          keepRepo)
+          .then(projectDeleted => {
+            this._handleCloseDeleteProject();
+            if (!projectDeleted) {
+              return;
+            }
+            page.show('/admin/projects/');
+          });
+    },
+  });
+})();
