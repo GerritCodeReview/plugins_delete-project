4d64eba07faae473a295179017df1bfa2754101d
Borui Tao <borui.tao@ericsson.com>
2018-02-28 11:12:36 -0500

    Introduce an archiving mechanism for deleted repos
    So far, if the preserveGitRepository option was set to false, once a
    repository was deleted, there was no way to recover it. Even if this
    makes sense, mistakes happen and it could be handy to have a backup
    mechanism when they occur. Besides, in some environments it could be
    desirable to give end users the permission to delete their own projects
    as this reduces the administrative tasks. In this case, being able to
    recover mistakenly deleted repositories could be even more critical.
    
    Introduce a new option to archive a repository instead of deleting it.
    This will allow the administrators to recover the git repositories in
    case of deletion by mistake. From the user point of view, and even for
    Gerrit, the repository is deleted as it is no longer available given it
    is moved to a different name and under a different, configurable named,
    folder.
    
    The archived repositories are kept for a configurable amount of time
    after which they are deleted; the auto-cleaning of the archived git
    repositories is done periodically, i.e., once a day.
    
    Three new configuration fields are added to customize the behaviour of
    this feature:
    
      - archiveDeletedRepos: whether to enable this feature. By default
        false.
      - archiveFolder: the path of the target archive folder. By default,
        the plugin data folder.
      - deleteArchivedReposAfter: the time period to keep the archived git
        repositories. By default, 180 days.
    
    Change-Id: I62985e8ffa5e3300c6927507e793dffd7aa2e54c

--- src/main/java/com/googlesource/gerrit/plugins/deleteproject/Configuration.java
+++ src/main/java/com/googlesource/gerrit/plugins/deleteproject/Configuration.java
@@ -14,33 +14,62 @@
 
 package com.googlesource.gerrit.plugins.deleteproject;
 
+import static java.util.concurrent.TimeUnit.DAYS;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.stream.Collectors.toList;
 
+import com.google.common.base.Strings;
+import com.google.gerrit.extensions.annotations.PluginData;
 import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.server.config.ConfigUtil;
 import com.google.gerrit.server.config.PluginConfig;
 import com.google.gerrit.server.config.PluginConfigFactory;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Singleton
 public class Configuration {
-  private static String DELETED_PROJECTS_PARENT = "Deleted-Projects";
+  private static final Logger log = LoggerFactory.getLogger(Configuration.class);
+  private static final String DELETED_PROJECTS_PARENT = "Deleted-Projects";
+  private static final long DEFAULT_ARCHIVE_DURATION_DAYS = 180;
 
   private final boolean allowDeletionWithTags;
+  private final boolean archiveDeletedRepos;
   private final boolean hideProjectOnPreserve;
+  private final long deleteArchivedReposAfter;
   private final String deletedProjectsParent;
+  private final Path archiveFolder;
   private final List<Pattern> protectedProjects;
+  private final PluginConfig cfg;
+
+  private File pluginData;
 
   @Inject
-  public Configuration(PluginConfigFactory pluginConfigFactory, @PluginName String pluginName) {
-    PluginConfig cfg = pluginConfigFactory.getFromGerritConfig(pluginName);
-    allowDeletionWithTags = cfg.getBoolean("allowDeletionOfReposWithTags", true);
-    hideProjectOnPreserve = cfg.getBoolean("hideProjectOnPreserve", false);
-    deletedProjectsParent = cfg.getString("parentForDeletedProjects", DELETED_PROJECTS_PARENT);
-    protectedProjects =
+  public Configuration(
+      PluginConfigFactory pluginConfigFactory,
+      @PluginName String pluginName,
+      @PluginData File pluginData) {
+    this.cfg = pluginConfigFactory.getFromGerritConfig(pluginName);
+    this.pluginData = pluginData;
+    this.allowDeletionWithTags = cfg.getBoolean("allowDeletionOfReposWithTags", true);
+    this.hideProjectOnPreserve = cfg.getBoolean("hideProjectOnPreserve", false);
+    this.deletedProjectsParent = cfg.getString("parentForDeletedProjects", DELETED_PROJECTS_PARENT);
+    this.archiveDeletedRepos = cfg.getBoolean("archiveDeletedRepos", false);
+    this.archiveFolder =
+        getArchiveFolderFromConfig(cfg.getString("archiveFolder", pluginData.toString()));
+    this.deleteArchivedReposAfter =
+        getArchiveDurationFromConfig(
+            Strings.nullToEmpty(cfg.getString("deleteArchivedReposAfter")));
+    this.protectedProjects =
         Arrays.asList(cfg.getStringList("protectedProject"))
             .stream()
             .map(Pattern::compile)
@@ -61,5 +90,43 @@ public class Configuration {
 
   public List<Pattern> protectedProjects() {
     return protectedProjects;
+  }
+
+  public boolean shouldArchiveDeletedRepos() {
+    return archiveDeletedRepos;
+  }
+
+  public Path getArchiveFolder() {
+    return archiveFolder;
+  }
+
+  public long getArchiveDuration() {
+    return deleteArchivedReposAfter;
+  }
+
+  private Path getArchiveFolderFromConfig(String configValue) {
+    try {
+      return Files.createDirectories(Paths.get(configValue));
+    } catch (Exception e) {
+      log.warn(
+          "Failed to create folder {}: {}; using default path: {}",
+          configValue,
+          e.getMessage(),
+          pluginData);
+      return pluginData.toPath();
+    }
+  }
+
+  private long getArchiveDurationFromConfig(String configValue) {
+    try {
+      return ConfigUtil.getTimeUnit(
+          configValue, DAYS.toMillis(DEFAULT_ARCHIVE_DURATION_DAYS), MILLISECONDS);
+    } catch (IllegalArgumentException e) {
+      log.warn(
+          "The configured archive duration is not valid: {}; using the default value: {} days",
+          e.getMessage(),
+          DEFAULT_ARCHIVE_DURATION_DAYS);
+      return DAYS.toMillis(DEFAULT_ARCHIVE_DURATION_DAYS);
+    }
   }
 }
