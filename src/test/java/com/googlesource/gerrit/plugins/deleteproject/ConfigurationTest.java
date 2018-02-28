3344b9792f2acfd5b98c7c86771aaa2bca8cc13a
Borui Tao <borui.tao@ericsson.com>
2018-02-28 11:12:36 -0500

    Introduce an archiving mechanism for deleted repos
    So far, if the preserveGitRepository option was set to false, once a
    repository was deleted, there was no way to recover it. Even if this
    makes sense, mistakes happen and it could be handy to have a backup
    mechanism when they occur. Besides, in some environments it could be
    desirable to give end users the permission to delete their own projects
    as this reduces the administrative tasks. In this case, being able to
    recover mistakenly deleted repositories could be even more critical.
    
    Introduce a new option to archive a repository instead of deleting it.
    This will allow the administrators to recover the git repositories in
    case of deletion by mistake. From the user point of view, and even for
    Gerrit, the repository is deleted as it is no longer available given it
    is moved to a different name and under a different, configurable named,
    folder.
    
    The archived repositories are kept for a configurable amount of time
    after which they are deleted; the auto-cleaning of the archived git
    repositories is done periodically, i.e., once a day.
    
    Three new configuration fields are added to customize the behaviour of
    this feature:
    
      - archiveDeletedRepos: whether to enable this feature. By default
        false.
      - archiveFolder: the path of the target archive folder. By default,
        the plugin data folder.
      - deleteArchivedReposAfter: the time period to keep the archived git
        repositories. By default, 180 days.
    
    Change-Id: I62985e8ffa5e3300c6927507e793dffd7aa2e54c

--- src/test/java/com/googlesource/gerrit/plugins/deleteproject/ConfigurationTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/deleteproject/ConfigurationTest.java
@@ -19,30 +19,52 @@ import static org.mockito.Mockito.when;
 
 import com.google.gerrit.server.config.PluginConfig;
 import com.google.gerrit.server.config.PluginConfigFactory;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.concurrent.TimeUnit;
 import org.eclipse.jgit.lib.Config;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ConfigurationTest {
+  private static final long DEFAULT_ARCHIVE_DURATION_MS = TimeUnit.DAYS.toMillis(180);
+  private static final String CUSTOM_DURATION = "100";
   private static final String CUSTOM_PARENT = "customParent";
+  private static final String INVALID_CUSTOM_FOLDER = "\0";
+  private static final String INVALID_ARCHIVE_DURATION = "180weeks180years";
   private static final String PLUGIN_NAME = "delete-project";
 
   @Mock private PluginConfigFactory pluginConfigFactoryMock;
+  @Rule public TemporaryFolder tempFolder = new TemporaryFolder();
 
+  private Path customArchiveFolder;
+  private File pluginDataDir;
   private Configuration deleteConfig;
+
+  @Before
+  public void setUp() throws Exception {
+    pluginDataDir = tempFolder.newFolder("data");
+    customArchiveFolder = tempFolder.newFolder("archive").toPath();
+  }
 
   @Test
   public void defaultValuesAreLoaded() {
     when(pluginConfigFactoryMock.getFromGerritConfig(PLUGIN_NAME))
         .thenReturn(new PluginConfig(PLUGIN_NAME, new Config()));
-    deleteConfig = new Configuration(pluginConfigFactoryMock, PLUGIN_NAME);
+    deleteConfig = new Configuration(pluginConfigFactoryMock, PLUGIN_NAME, pluginDataDir);
 
     assertThat(deleteConfig.getDeletedProjectsParent()).isEqualTo("Deleted-Projects");
     assertThat(deleteConfig.deletionWithTagsAllowed()).isTrue();
     assertThat(deleteConfig.projectOnPreserveHidden()).isFalse();
+    assertThat(deleteConfig.shouldArchiveDeletedRepos()).isFalse();
+    assertThat(deleteConfig.getArchiveDuration()).isEqualTo(DEFAULT_ARCHIVE_DURATION_MS);
+    assertThat(deleteConfig.getArchiveFolder().toString()).isEqualTo(pluginDataDir.toString());
   }
 
   @Test
@@ -51,11 +73,53 @@ public class ConfigurationTest {
     pluginConfig.setString("parentForDeletedProjects", CUSTOM_PARENT);
     pluginConfig.setBoolean("allowDeletionOfReposWithTags", false);
     pluginConfig.setBoolean("hideProjectOnPreserve", true);
+    pluginConfig.setBoolean("archiveDeletedRepos", true);
+    pluginConfig.setString("deleteArchivedReposAfter", CUSTOM_DURATION);
+    pluginConfig.setString("archiveFolder", customArchiveFolder.toString());
+
     when(pluginConfigFactoryMock.getFromGerritConfig(PLUGIN_NAME)).thenReturn(pluginConfig);
-    deleteConfig = new Configuration(pluginConfigFactoryMock, PLUGIN_NAME);
+    deleteConfig = new Configuration(pluginConfigFactoryMock, PLUGIN_NAME, pluginDataDir);
 
     assertThat(deleteConfig.getDeletedProjectsParent()).isEqualTo(CUSTOM_PARENT);
     assertThat(deleteConfig.deletionWithTagsAllowed()).isFalse();
     assertThat(deleteConfig.projectOnPreserveHidden()).isTrue();
+    assertThat(deleteConfig.shouldArchiveDeletedRepos()).isTrue();
+    assertThat(deleteConfig.getArchiveDuration()).isEqualTo(Long.parseLong(CUSTOM_DURATION));
+    assertThat(deleteConfig.getArchiveFolder().toString())
+        .isEqualTo(customArchiveFolder.toString());
+  }
+
+  @Test
+  public void archiveDurationWithUnitIsLoaded() {
+    PluginConfig pluginConfig = new PluginConfig(PLUGIN_NAME, new Config());
+    pluginConfig.setString("deleteArchivedReposAfter", CUSTOM_DURATION + "years");
+
+    when(pluginConfigFactoryMock.getFromGerritConfig(PLUGIN_NAME)).thenReturn(pluginConfig);
+    deleteConfig = new Configuration(pluginConfigFactoryMock, PLUGIN_NAME, pluginDataDir);
+
+    assertThat(deleteConfig.getArchiveDuration())
+        .isEqualTo(TimeUnit.DAYS.toMillis(Long.parseLong(CUSTOM_DURATION)) * 365);
+  }
+
+  @Test
+  public void invalidArchiveDuration() {
+    PluginConfig pluginConfig = new PluginConfig(PLUGIN_NAME, new Config());
+    pluginConfig.setString("deleteArchivedReposAfter", INVALID_ARCHIVE_DURATION);
+
+    when(pluginConfigFactoryMock.getFromGerritConfig(PLUGIN_NAME)).thenReturn(pluginConfig);
+    deleteConfig = new Configuration(pluginConfigFactoryMock, PLUGIN_NAME, pluginDataDir);
+
+    assertThat(deleteConfig.getArchiveDuration()).isEqualTo(DEFAULT_ARCHIVE_DURATION_MS);
+  }
+
+  @Test
+  public void invalidTargetArchiveFolder() {
+    PluginConfig pluginConfig = new PluginConfig(PLUGIN_NAME, new Config());
+    pluginConfig.setString("archiveFolder", INVALID_CUSTOM_FOLDER);
+
+    when(pluginConfigFactoryMock.getFromGerritConfig(PLUGIN_NAME)).thenReturn(pluginConfig);
+    deleteConfig = new Configuration(pluginConfigFactoryMock, PLUGIN_NAME, pluginDataDir);
+
+    assertThat(deleteConfig.getArchiveFolder().toString()).isEqualTo(pluginDataDir.toString());
   }
 }
