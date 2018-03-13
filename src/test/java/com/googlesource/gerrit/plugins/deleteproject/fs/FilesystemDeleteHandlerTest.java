a2cfa48fd906d1fe8ef06669587d42c3479b8c6c
Borui Tao <borui.tao@ericsson.com>
2018-03-13 10:04:21 -0400

    Add IT tests and modify unit tests to achieve higher coverage
    Change-Id: I28300bd3e1a606b99f1f902eccc691f8681900d8

---
+++ src/test/java/com/googlesource/gerrit/plugins/deleteproject/fs/FilesystemDeleteHandlerTest.java
@@ -0,0 +1,66 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.deleteproject.fs;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FilesystemDeleteHandlerTest {
+  private static final int NUMBER_OF_FOLDERS = 10;
+
+  private FilesystemDeleteHandler fsDeleteHandler;
+  private Path projectRepo;
+
+  @Rule public TemporaryFolder tempFolder = new TemporaryFolder();
+
+  @Before
+  public void setUp() throws Exception {
+    fsDeleteHandler = new FilesystemDeleteHandler(null, null, null);
+    projectRepo = tempFolder.newFolder("project").toPath();
+  }
+
+  @Test
+  public void recursiveDeleteTest() throws IOException {
+    assertThat(isDirEmpty(projectRepo)).isTrue();
+
+    projectRepo.resolve("myPj").toFile().mkdir();
+    for (int i = 0; i < NUMBER_OF_FOLDERS; i++) {
+      projectRepo.resolve("myPj" + i).toFile().mkdir();
+    }
+    for (int i = 0; i < NUMBER_OF_FOLDERS; i++) {
+      projectRepo.resolve("newFile" + i + "txt").toFile().createNewFile();
+    }
+    assertThat(isDirEmpty(projectRepo)).isFalse();
+    fsDeleteHandler.recursiveDelete(projectRepo);
+    assertThat(projectRepo.toFile().exists()).isFalse();
+  }
+
+  private boolean isDirEmpty(final Path dir) throws IOException {
+    try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(dir)) {
+      return !dirStream.iterator().hasNext();
+    }
+  }
+}
