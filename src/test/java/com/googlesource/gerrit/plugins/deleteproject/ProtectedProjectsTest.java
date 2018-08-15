ef1d4c00fc7741a49c67268f4b3808fd53185355
Hector Oswaldo Caballero <hector.caballero@ericsson.com>
2018-08-15 05:21:04 -0400

    Add tests for protected projects
    Change-Id: I535158100f94aa49cb13a053c5fd364709ea02f6

---
+++ src/test/java/com/googlesource/gerrit/plugins/deleteproject/ProtectedProjectsTest.java
@@ -0,0 +1,75 @@
+// Copyright (C) 2018 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.deleteproject;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.when;
+
+import com.google.gerrit.reviewdb.client.Project;
+import com.google.gerrit.server.config.AllProjectsName;
+import com.google.gerrit.server.config.AllProjectsNameProvider;
+import com.google.gerrit.server.config.AllUsersName;
+import com.google.gerrit.server.config.AllUsersNameProvider;
+import com.google.gerrit.server.config.PluginConfig;
+import com.google.gerrit.server.config.PluginConfigFactory;
+import org.eclipse.jgit.lib.Config;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ProtectedProjectsTest {
+  private static final String PLUGIN_NAME = "delete-project";
+
+  @Mock private AllProjectsNameProvider allProjectsMock;
+  @Mock private AllUsersNameProvider allUsersMock;
+  @Mock private PluginConfigFactory pluginConfigFactoryMock;
+
+  private ProtectedProjects protectedProjects;
+
+  @Before
+  public void setup() throws Exception {
+    when(allProjectsMock.get()).thenReturn(new AllProjectsName("All-Projects"));
+    when(allUsersMock.get()).thenReturn(new AllUsersName("All-Users"));
+    PluginConfig pluginConfig = new PluginConfig(PLUGIN_NAME, new Config());
+    when(pluginConfigFactoryMock.getFromGerritConfig(PLUGIN_NAME)).thenReturn(pluginConfig);
+    protectedProjects = new ProtectedProjects(allProjectsMock, allUsersMock);
+  }
+
+  @Test
+  public void allProjectsIsProtected() throws Exception {
+    assertProtected("All-Projects");
+  }
+
+  @Test
+  public void allUsersIsProtected() throws Exception {
+    assertProtected("All-Users");
+  }
+
+  @Test
+  public void otherProjectIsNotProtected() throws Exception {
+    assertNotProtected("test-project");
+  }
+
+  private void assertProtected(String name) {
+    assertThat(protectedProjects.isProtected(new Project.NameKey(name))).isTrue();
+  }
+
+  private void assertNotProtected(String name) {
+    assertThat(protectedProjects.isProtected(new Project.NameKey(name))).isFalse();
+  }
+}
