51ea7e286012dcec3d12d8b65b1689f21333c555
Paladox none <thomasmulhall410@yahoo.com>
2019-01-04 16:52:31 +0000

    test: do not merge
    Change-Id: I96ea3cf03ef75139061c14388e197b2af4db2063

--- src/main/resources/static/delete-project.js
+++ src/main/resources/static/delete-project.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-if (!window.Polymer) {
+if (!window.Polymer || Polymer) {
   Gerrit.install(function(self) {
       function onDeleteProject(c) {
         var f = c.checkbox();
