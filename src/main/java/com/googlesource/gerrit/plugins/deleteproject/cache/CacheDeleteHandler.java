c06a7e4d9b63dddbc3575317d0b989ef69c81fe0
Edwin Kempin <ekempin@google.com>
2020-03-11 11:25:20 +0100

    Adapt the change in Gerrit core
    Since change I9037aa7a3 ProjectCache#remove no longer throws
    IOException.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: Ifa2c8294da3c639aedfb171ff28d9a426cf02221

--- src/main/java/com/googlesource/gerrit/plugins/deleteproject/cache/CacheDeleteHandler.java
+++ src/main/java/com/googlesource/gerrit/plugins/deleteproject/cache/CacheDeleteHandler.java
@@ -17,7 +17,6 @@ package com.googlesource.gerrit.plugins.deleteproject.cache;
 import com.google.gerrit.entities.Project;
 import com.google.gerrit.server.project.ProjectCache;
 import com.google.inject.Inject;
-import java.io.IOException;
 
 public class CacheDeleteHandler {
 
@@ -28,7 +27,7 @@ public class CacheDeleteHandler {
     this.projectCache = projectCache;
   }
 
-  public void delete(Project project) throws IOException {
+  public void delete(Project project) {
     projectCache.remove(project);
   }
 }
