8053b78733a113258462ebf96efc96561ffa444b
David Ostrovsky <david@ostrovsky.org>
2020-03-22 09:27:51 +0100

    Migrate delete-project plugin to polymer 3
    Feature: Issue 12535
    Change-Id: I6f92b9d874c32b67c5c3f274b9a83e6795655ba3

--- gr-delete-repo/gr-delete-repo.js
+++ gr-delete-repo/gr-delete-repo.js
@@ -1,70 +1,81 @@
-// Copyright (C) 2018 The Android Open Source Project
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-(function() {
-  'use strict';
+/**
+ * @license
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-  class GrDeleteRepo extends Polymer.GestureEventListeners(
-      Polymer.LegacyElementMixin(
-          Polymer.Element)) {
-    static get is() { return 'gr-delete-repo'; }
+import {htmlTemplate} from './gr-delete-repo_html.js';
 
-    static get properties() {
-      return {
-        repoName: String,
-        config: Object,
-        action: Object,
-        actionId: String,
-      };
-    }
+class GrDeleteRepo extends Polymer.GestureEventListeners(
+    Polymer.LegacyElementMixin(
+        Polymer.Element)) {
+  /** @returns {string} name of the component */
+  static get is() { return 'gr-delete-repo'; }
 
-    attached() {
-      super.attached();
-      this.actionId = this.plugin.getPluginName() + '~delete';
-      this.action = this.config.actions[this.actionId];
-      this.hidden = !this.action;
-    }
+  /** @returns {?} template for this component */
+  static get template() { return htmlTemplate; }
 
-    _handleCommandTap() {
-      this.$.deleteRepoOverlay.open();
-    }
-
-    _handleCloseDeleteRepo() {
-      this.$.deleteRepoOverlay.close();
-    }
-
-    _handleDeleteRepo() {
-      const endpoint = '/projects/' +
-          encodeURIComponent(this.repoName) + '/' +
-          this.actionId;
-
-      const json = {
-        force: this.$.forceDeleteOpenChangesCheckBox.checked,
-        preserve: this.$.preserveGitRepoCheckBox.checked,
-      };
-
-      const errFn = response => {
-        this.fire('page-error', {response});
-      };
-
-      return this.plugin.restApi().send(
-          this.action.method, endpoint, json, errFn)
-          .then(r => {
-            this.plugin.restApi().invalidateReposCache();
-            Gerrit.Nav.navigateToRelativeUrl('/admin/repos');
-          });
-    }
+  /**
+   * Defines properties of the component
+   *
+   * @returns {?}
+   */
+  static get properties() {
+    return {
+      repoName: String,
+      config: Object,
+      action: Object,
+      actionId: String,
+    };
   }
 
-  customElements.define(GrDeleteRepo.is, GrDeleteRepo);
-})();
+  attached() {
+    super.attached();
+    this.actionId = this.plugin.getPluginName() + '~delete';
+    this.action = this.config.actions[this.actionId];
+    this.hidden = !this.action;
+  }
+
+  _handleCommandTap() {
+    this.$.deleteRepoOverlay.open();
+  }
+
+  _handleCloseDeleteRepo() {
+    this.$.deleteRepoOverlay.close();
+  }
+
+  _handleDeleteRepo() {
+    const endpoint = '/projects/' +
+        encodeURIComponent(this.repoName) + '/' +
+        this.actionId;
+
+    const json = {
+      force: this.$.forceDeleteOpenChangesCheckBox.checked,
+      preserve: this.$.preserveGitRepoCheckBox.checked,
+    };
+
+    const errFn = response => {
+      this.fire('page-error', {response});
+    };
+
+    return this.plugin.restApi().send(
+        this.action.method, endpoint, json, errFn)
+        .then(r => {
+          this.plugin.restApi().invalidateReposCache();
+          Gerrit.Nav.navigateToRelativeUrl('/admin/repos');
+        });
+  }
+}
+
+customElements.define(GrDeleteRepo.is, GrDeleteRepo);
--- gr-delete-repo/gr-delete-repo_html.js
+++ gr-delete-repo/gr-delete-repo_html.js
@@ -1,21 +1,21 @@
-<!--
-Copyright (C) 2018 The Android Open Source Project
+/**
+ * @license
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
--->
-
-<dom-module id="gr-delete-repo">
-  <template>
+export const htmlTemplate = Polymer.html`
     <style include="gr-form-styles"></style>
     <gr-repo-command
         title="[[action.label]]"
@@ -52,6 +52,4 @@ limitations under the License.
         </div>
       </gr-dialog>
     </gr-overlay>
-  </template>
-  <script src="gr-delete-repo.js"></script>
-</dom-module>
+`;
