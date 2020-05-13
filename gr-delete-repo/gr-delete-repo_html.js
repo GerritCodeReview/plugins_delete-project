bd0ad4b9acb5183a9d43795a55d84ebc54483777
Paladox none <thomasmulhall410@yahoo.com>
2020-05-13 18:03:29 +0000

    Change htmlTemplate to be a let
    htmlTemplate seems to be conflicting with checks version.
    
    Change-Id: I0d9f21f301b5a738f063f4c24d1f1dd97d34c19a

--- gr-delete-repo/gr-delete-repo_html.js
+++ gr-delete-repo/gr-delete-repo_html.js
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-export const htmlTemplate = Polymer.html`
+export let htmlTemplate = Polymer.html`
     <style include="gr-form-styles"></style>
     <gr-repo-command
         title="[[action.label]]"
