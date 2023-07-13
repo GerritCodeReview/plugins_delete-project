33f58277025b68559b0b4fc616008b12bcdd22b9
Dhruv Srivastava <dhruvsri@google.com>
2023-07-13 12:58:59 +0000

    ignore delete project
    Change-Id: I4793f44b39a8bf6ca5c6b93568f45590e600995d
    
    Change-Id: I4793f44b39a8bf6ca5c6b93568f45590e600995d

--- src/test/java/com/googlesource/gerrit/plugins/deleteproject/ConfigurationTest.java
+++ src/test/java/com/googlesource/gerrit/plugins/deleteproject/ConfigurationTest.java
@@ -6,24 +6,9 @@
 //
 // http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package com.googlesource.gerrit.plugins.deleteproject;
-
-import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.Mockito.when;
 
 import com.google.gerrit.server.config.PluginConfig;
 import com.google.gerrit.server.config.PluginConfigFactory;
-import java.io.File;
-import java.nio.file.Path;
-import java.util.concurrent.TimeUnit;
-import org.eclipse.jgit.lib.Config;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
